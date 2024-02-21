9e8cb7a833efd52edfaa8193af770d47ed499e13
EC2 Default User <ec2-user@ip-172-31-25-136.ap-south-1.compute.internal>
2024-02-21 07:09:23 +0000

    first commit

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'; 
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import { CodePipeline } from 'aws-cdk-lib/pipelines';
+
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    new CodePipeline(this, 'firstPipeline',{
+      pipelineName:'firstPipeline',
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub()
+      })
+    })
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
